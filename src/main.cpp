412636a35a5f1ba94adbf0ffb6099c378f585504
Daniel <166765631+daaburto@users.noreply.github.com>
2025-04-29 17:42:22 -0400

    fix: implementación de stack

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-#include <deque>
+#include <stack>
 #include "heroe.h"
 
 using namespace std;
 
+/*
+Implementación del problema usando el ADT Stack.
+Se eligió trabajar con un Stack porque consideramos que las operaciones que tiene son suficientes para
+satisfacer el problema planteado.
+En específico, para los CANOs es más fácil trabajar con los métodos de un Stack que con los del Queue, ya que,
+a diferencia de un Queue, el Stack ingresa los nuevos esbirros desde arriba, dejándolos encima del esbirro
+anterior y listos para el siguiente ataque, mientras que en Queue tendríamos que dar la vuelta a la cola para
+colocar a los nuevos esbirros en la cima.
+*/
 int main() {
     int vida_mechon, cant_esbirros;
     cin >> vida_mechon;
@@ -33,29 +42,29 @@ int main() {
 
     // Inicializar clases
     Heroe mechon(vida_mechon);
-    deque<Esbirro> esbirros; // deque que va a contener a los esbirros
-    for (int i = 0; i < cant_esbirros; i++) {
-        esbirros.push_back(Esbirro(vida_esbirro[i], ataque_esbirro[i], esbirro_cano[i]));
+    stack<Esbirro> esbirros; // stack que va a contener a los esbirros
+    for (int i = cant_esbirros - 1; i >= 0; i--) {
+        esbirros.push(Esbirro(vida_esbirro[i], ataque_esbirro[i], esbirro_cano[i]));
     }
 
     // Simulación del enfrentamiento
     while (!esbirros.empty() && !mechon.estaMuerto()) {
-        Esbirro &esbirro_actual = esbirros.front();
+        Esbirro &esbirro_actual = esbirros.top();
         mechon.atacar(esbirro_actual);
 
         // Verifica si el esbirro está muerto para quitarlo del deque
         if (esbirro_actual.estaMuerto()) {
             mechon.eliminarEsbirro();
-            esbirros.pop_front();
+            esbirros.pop();
         }
 
         // Si aún quedan esbirros, verifica si el esbirro CANO puede separarse
         if (!esbirros.empty()) {
             if (esbirro_actual.getEsCano() && esbirro_actual.getAtaquesRecibidos() == 2) {
-                Esbirro cano = esbirros.front();
-                esbirros.pop_front();
-                esbirros.push_front(cano.crearEsbirro());
-                esbirros.push_front(cano.crearEsbirro());
+                Esbirro cano = esbirros.top();
+                esbirros.pop();
+                esbirros.push(cano.crearEsbirro());
+                esbirros.push(cano.crearEsbirro());
             }
             // mechón recibe ataque del esbirro en la primera posición del deque
             mechon.recibirAtaque(esbirro_actual.getAtaque());
